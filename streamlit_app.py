45a22e218080da61d23c9a5a4c389a1225972016
CrunchWillSupreme <33377681+CrunchWillSupreme@users.noreply.github.com>
2022-05-26 17:57:30 -0400

    Update streamlit_app.py
    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-x = 'this is testing text'
-print(x)
+streamlit.text('This is testing text')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
